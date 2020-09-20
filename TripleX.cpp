b92a48dd26f5b965586a8c1b416a26fbee10225d
Brian Levin <bml201095@gmail.com>
2020-09-20 03:10:40 -0400

    first line of c++ code

---
+++ TripleX.cpp
@@ -0,0 +1,5 @@
+int main(){ // int main required in every c++ program
+    return 0; //    return 0= program has run successfully, compiler ignores white space. g++ triplex.cpp -o triplex, ./triplex
+
+ 
+}
