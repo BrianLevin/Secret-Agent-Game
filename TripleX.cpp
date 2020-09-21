2a123417688f48597c2a0202686b0571d2254710
Brian Levin <bml201095@gmail.com>
2020-09-20 22:35:20 -0400

    declared variables and learned how to make use of mult and div operators with line breaks

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,5 +1,28 @@
+#include <iostream> // allow us to write the text to the terminal from the library ## include a proporoccer to include a library of code stream is a library of code
+
 int main(){ // int main required in every c++ program
-    return 0; //    return 0= program has run successfully, compiler ignores white space. g++ triplex.cpp -o triplex, ./triplex
+// std short for standard is the name space for the function :: check for code inside the nae space, cout allows to write  to the terminal, << allows output from the terminal, "is a string" whole line is called the expression statement
+std::cout << "You are a secret agent breaking into a secure service room";// name space, scope operator, cout allows output, expression stateme
+std::cout <<  std::endl; // line break
+std::cout << "Enter the correct code to continue..."; 
+
+
+     int a = 4;
+     int b = 3;
+     int c = 2;
+
+     int sum = a+ b +c;
+
+     int product = a*b*c;
+
+     std::cout << std::endl;
+     std::cout <<  sum << std::endl;
+     std::cout << product << std::endl; 
+     
+    return 0; //    return 0= program has ru +bn successfully, compiler ignores white space. g++ triplex.cpp -o triplex, ./triplex
+
 
  
 }
+
+// g++
