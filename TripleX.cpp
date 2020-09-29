dfe54c78fd8b1bf915007c914b65e883879ad71d
Brian Levin <bml201095@gmail.com>
2020-09-28 23:19:12 -0400

    Naming and self document the variables in the code

--- TripleX.cpp
+++ TripleX.cpp
@@ -9,22 +9,24 @@ std::cout << "Enter the correct code to continue...";  // expression statement a
 
 
 
-   const  int a = 4; // const variable dont chage
-     const int b = 3;
-     const int c = 2;
+   const  int CodeA = 4; // const variable dont chage
+     const int CodeB = 3;
+     const int CodeC = 2;
 
 // expression followed by a semi colin is a semi statement
 // declaration statements  where we declare somthing
 
-    const  int sum = a+ b +c; // sum varable
+    const  int sum = CodeA+ CodeB +CodeC; // sum varable
 
-      const int product = a*b*c; // mult variable // declariative staement
+      const int product = CodeA * CodeB * CodeC; // mult variable // declariative staement
 
      std::cout << std::endl; // line break
-     std::cout <<  sum << std::endl;  // print sum // expression staement
+     std::cout << "+ There are three numbers in the code" << std::endl;
+     std::cout <<  "+ The codes add  up to: " <<  sum << std::endl;  // print sum // expression staement
+     std::cout << "+ The codes Multiply to give: " << product << std::endl; 
      std::cout << product << std::endl;  // print product
 
-     
+     int PlayerGuess;
      
     return 0; //    return 0= program has ru +bn successfully, compiler ignores white space. g++ triplex.cpp -o triplex, ./triplex
 // return statement
