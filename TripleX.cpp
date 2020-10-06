a07036353523504113d424f564002d8d116756e0
Brian Levin <bml201095@gmail.com>
2020-10-05 23:33:08 -0400

    worked with function paramters and scoping

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,17 +1,18 @@
 #include <iostream> // allow us to write the text to the terminal from the library ## include a proporoccer to include a library of code stream is a library of code
 // preprocessor directive
 
-void PrintIntroduction() // | \n new line sequence in strings, a function is a group of staements that is named  and performs a task, must a  stepcify a return type in a function , void to return no data.
+void PrintIntroduction(  int Difficulty)  // int difficulty is the parameter so the variable can have scope
+// | \n new line sequence in strings, a function is a group of staements that is named  and performs a task, must a  stepcify a return type in a function , void to return no data.
 {
-std::cout << "\n\nYou are a secret agent breaking into a secure server room...\n";
-std::cout << "Enter the correct code to continue... \n\n";
+std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty; // scope
+std::cout << " secure server room...\nEnter the correct code to continue... \n\n";
 
 
 }
 // return a boolan value
-bool PlayGame()//  void retrn type //name PlayGame // void means it will return no data functions are a group of staements that perform a task with a name
+bool PlayGame(int Difficulty)//  void retrn type //name PlayGame // void means it will return no data functions are a group of staements that perform a task with a name
 {
-  PrintIntroduction();
+  PrintIntroduction( Difficulty); // scope starts out defined as an int
 
 //std::cout << "You are a secret agent breaking into a secure service room...\n";// name space, scope operator, cout allows output, expression stateme
 //std::cout <<  std::endl; // line break
@@ -68,13 +69,20 @@ return false;
 
 int main(){ // int main required in every c++ program //main function
 // std short for standard is the name space for the function :: check for code inside the nae space, cout allows to write  to the terminal, << allows output from the terminal, "is a string" whole line is called the expression statement
-
+int LevelDifficulty = 1; //intialize outside while loop 
 // chracters like x get converted to 0
      // program will only ask for more cin if input stream is empty
      while(true){ // while the condition is true, execute the function
-        bool bLevelComplete= PlayGame(); // if the player has completed the level
+        bool bLevelComplete= PlayGame(LevelDifficulty); // if the player has completed the level
        std::cin.clear();// clear any errors  //  ignres the erro to prevent endless loop
        std::cin.ignore();// discards the buffer // helps prevent endless loop if a wrong character is is put in
+
+       if (bLevelComplete){
+       //increase level difficultu
+       ++LevelDifficulty; //increment variable
+         
+       }
+       
      }
 
      
@@ -83,7 +91,7 @@ int main(){ // int main required in every c++ program //main function
 // return statement
  
 }
-
+// think about where you declare vairables because of scope
 // g++
 // std::cin >> player game
 //cin= character intput
@@ -92,7 +100,8 @@ int main(){ // int main required in every c++ program //main function
 //cin = character input
 // >> extration operator = >>
 // extract from input stream and assign to  variable: "player guess"
-
+// passing values is passing arguments
 // == equality operator
-
+// functions can have parameteors whch get passed s arguments
 // && logical and operator
+// things must be defined in the  scope
